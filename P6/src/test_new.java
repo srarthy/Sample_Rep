f51088c71221f1c3ed2ec8dceeb367a3b372a199
Arthy <Arthy@DESKTOP-L4HUAOJ>
2020-08-21 10:16:24 -0700

    Addition

--- P6/src/test_new.java
+++ P6/src/test_new.java
@@ -2,7 +2,7 @@
 public class test_new {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("welcome to Java Selenium nnnnnnnmmmmm"); 
+		System.out.println("welcome to Java Selenium nnnnnnnmmmmm.Love working hard"); 
 	}
 
 }
