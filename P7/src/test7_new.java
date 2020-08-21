cf1767a4e16965fd15e00f341818549d6e19b35b
Arthy <Arthy@DESKTOP-L4HUAOJ>
2020-08-21 10:25:25 -0700

    Please work

---
+++ P7/src/test7_new.java
@@ -0,0 +1,9 @@
+
+public class test7_new {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to Java Selenium Love working hard and making mistakes");
+	}
+
+}
