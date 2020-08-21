0f4646013a17b4f27614938c63a6fdaea076bfd2
Arthy <Arthy@DESKTOP-L4HUAOJ>
2020-08-21 11:04:17 -0700

    One line added

--- P7/src/test7_new.java
+++ P7/src/test7_new.java
@@ -3,7 +3,7 @@ public class test7_new {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("welcome to Java Selenium Love working hard and making mistakes");
+		System.out.println("welcome to Java Selenium Love working hard and making mistakes...One line added");
 	}
 
 }
